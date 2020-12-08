cc97c14b682a5a68cd2aa6774cf695c9fd395149
Al Joseph <condinoaljoseph@gmail.com>
2020-12-08 11:33:49 +0800

    transpiled from ts

--- src/algorithms/math/calc_avg_height/index.js
+++ src/algorithms/math/calc_avg_height/index.js
@@ -1,15 +1,12 @@
 function calcAvgHeight(data) {
-	const people = Object.keys(data);
-	const isEmpty = people.length === 0 && data.constructor === Object;
-	let aveHeight = 0;
-
-	if (isEmpty) return null;
-
-	people.forEach((person) => {
-		aveHeight += data[person].height;
-	});
-
-	return aveHeight / people.length;
+    var people = Object.keys(data); // ['Matt', 'Jason']
+    var isEmpty = people.length === 0 && data.constructor === Object;
+    var aveHeight = 0;
+    if (isEmpty)
+        return null;
+    people.forEach(function (person) {
+        aveHeight += data[person].height;
+    });
+    return aveHeight / people.length;
 }
-
 module.exports = calcAvgHeight;
