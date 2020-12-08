ac8eb406c2a862808b7608814f835c426b5ea0a2
Al Joseph <condinoaljoseph@gmail.com>
2020-12-08 09:45:36 +0800

    solution

--- src/algorithms/math/calc_avg_height/index.js
+++ src/algorithms/math/calc_avg_height/index.js
@@ -1,3 +1,15 @@
 function calcAvgHeight(data) {
-	return "hello";
+	const people = Object.keys(data);
+	const isEmpty = people.length === 0 && data.constructor === Object;
+	let aveHeight = 0;
+
+	if (isEmpty) return null;
+
+	people.forEach((person) => {
+		aveHeight += data[person].height;
+	});
+
+	return aveHeight / 2;
 }
+
+module.exports = calcAvgHeight;
